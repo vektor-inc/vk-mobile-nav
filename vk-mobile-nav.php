decf2f6b6b8195cabb20b4dfeafeb42c078d78c2
catherine <catherine5th.avenue@gmail.com>
2017-09-28 15:25:13 +0900

    こみっと

---
+++ vk-mobile-nav.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Vk Mobile Nav
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     vk-mobile-nav
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Vk_Mobile_Nav
+ */
